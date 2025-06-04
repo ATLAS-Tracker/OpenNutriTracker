3895b8f68187f25a18ce49529921bdf5ce80f607
Pierre Lammers <100353085+pierre-lammers@users.noreply.github.com>
2025-06-04 12:15:19 -0400

    refactor: use MealOrRecipeEntity enum

--- lib/features/meal_detail/presentation/widgets/meal_detail_bottom_sheet.dart
+++ lib/features/meal_detail/presentation/widgets/meal_detail_bottom_sheet.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 import 'package:opennutritracker/core/domain/entity/intake_type_entity.dart';
 import 'package:opennutritracker/core/utils/locator.dart';
 import 'package:opennutritracker/features/add_meal/domain/entity/meal_entity.dart';
+import 'package:opennutritracker/features/add_meal/domain/entity/meal_or_recipe_entity.dart';
 import 'package:opennutritracker/features/diary/presentation/bloc/calendar_day_bloc.dart';
 import 'package:opennutritracker/features/diary/presentation/bloc/diary_bloc.dart';
 import 'package:opennutritracker/features/home/presentation/bloc/home_bloc.dart';
@@ -85,7 +86,7 @@ class MealDetailBottomSheet extends StatelessWidget {
                             child: DropdownButtonFormField<String>(
                               isExpanded: true,
                               value: (product.mealOrRecipe != null &&
-                                      product.mealOrRecipe == "recipe")
+                                      product.mealOrRecipe == MealOrRecipeEntity.recipe)
                                   ? UnitDropdownItem.serving.toString()
                                   : selectedUnit,
                               decoration: InputDecoration(
@@ -93,7 +94,7 @@ class MealDetailBottomSheet extends StatelessWidget {
                                 labelText: S.of(context).unitLabel,
                               ),
                               items: (product.mealOrRecipe != null &&
-                                      product.mealOrRecipe == "recipe")
+                                      product.mealOrRecipe == MealOrRecipeEntity.recipe)
                                   ? [_getServingDropdownItem(context)]
                                   : <DropdownMenuItem<String>>[
                                       if (product.hasServingValues)
