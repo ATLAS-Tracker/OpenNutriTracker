05475c39350abbdcf9cc3881506efa429803cbab
Pierre Lammers <lammers.pierre@gmail.com>
2025-06-21 14:53:35 +0200

    fix: locator order

--- lib/core/utils/locator.dart
+++ lib/core/utils/locator.dart
@@ -64,21 +64,22 @@ import 'package:opennutritracker/features/settings/domain/usecase/export_data_us
 import 'package:opennutritracker/features/settings/domain/usecase/import_data_usecase.dart';
 import 'package:opennutritracker/features/settings/presentation/bloc/export_import_bloc.dart';
 import 'package:opennutritracker/features/settings/presentation/bloc/settings_bloc.dart';
+import 'package:opennutritracker/features/sync/supabase_client.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 final locator = GetIt.instance;
 
 Future<void> initLocator() async {
+  // Backend
+  await Supabase.initialize(
+      url: Env.supabaseProjectUrl, anonKey: Env.supabaseProjectAnonKey);
+  locator.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
+
   // Init secure storage and Hive database;
   final secureAppStorageProvider = SecureAppStorageProvider();
   final hiveDBProvider = HiveDBProvider();
   await hiveDBProvider
       .initHiveDB(await secureAppStorageProvider.getHiveEncryptionKey());
-
-  // Backend
-  await Supabase.initialize(
-      url: Env.supabaseProjectUrl, anonKey: Env.supabaseProjectAnonKey);
-  locator.registerLazySingleton<SupabaseClient>(() => Supabase.instance.client);
 
   // Cache manager
   locator
