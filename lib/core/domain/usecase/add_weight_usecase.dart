83105fb462b22cf4f1b1560a3df09d0d62162409
Pierre Lammers <100353085+pierre-lammers@users.noreply.github.com>
2025-07-05 13:11:34 +0200

    Update last update timestamps automatically

--- lib/core/domain/usecase/add_weight_usecase.dart
+++ lib/core/domain/usecase/add_weight_usecase.dart
@@ -1,12 +1,15 @@
 import 'package:opennutritracker/core/data/repository/user_weight_repository.dart';
+import 'package:opennutritracker/core/data/repository/config_repository.dart';
 import 'package:opennutritracker/core/domain/entity/user_weight_entity.dart';
 
 class AddWeightUsecase {
   final UserWeightRepository _userWeightRepository;
+  final ConfigRepository _configRepository;
 
-  AddWeightUsecase(this._userWeightRepository);
+  AddWeightUsecase(this._userWeightRepository, this._configRepository);
 
   Future<void> addUserWeight(UserWeightEntity userWeightEntity) async {
-    return await _userWeightRepository.addUserWeight(userWeightEntity);
+    await _userWeightRepository.addUserWeight(userWeightEntity);
+    await _configRepository.setUserWeightLastUpdate(DateTime.now());
   }
 }
