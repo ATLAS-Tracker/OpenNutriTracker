6b1453d0a5c439ba79cbb170c9614e6a7bf10587
b-chollet <benjchollet@gmail.com>
2025-08-06 15:33:21 +0200

    feat: Add new logos and remove old ones

--- lib/core/presentation/widgets/dynamic_ont_logo.dart
+++ lib/core/presentation/widgets/dynamic_ont_logo.dart
@@ -5,32 +5,34 @@ import 'package:opennutritracker/core/utils/extensions.dart';
 class DynamicOntLogo extends StatelessWidget {
   const DynamicOntLogo({super.key});
 
-  static const _circleColor = 'circleColor';
-  static const _spoonColor = 'spoonColor';
+  static const logoAcolorLabel = "Acolor";
+  static const logoTcolorLabel = "Tcolor";
 
   @override
   Widget build(BuildContext context) {
+    String logoAcolor = Theme.of(context).colorScheme.primaryContainer.toHex();
+    String logoTcolor = Theme.of(context).colorScheme.primaryContainer.toHex();
+
     return FutureBuilder<String>(
       future: DefaultAssetBundle.of(context)
-          .loadString('assets/icon/ont_logo_square.svg'),
+          .loadString('assets/icon/ont_logo_atlas_tracker.svg'),
       builder: (context, snapshot) {
         if (snapshot.connectionState == ConnectionState.done &&
             snapshot.hasData) {
           String? svgString = snapshot.data!;
 
-          // Replace the placeholders with desired colors.
-          svgString = svgString.replaceAll(_circleColor,
-              Theme.of(context).colorScheme.primaryContainer.toHex());
-          svgString = svgString.replaceAll(
-              _spoonColor, Theme.of(context).colorScheme.onSurface.toHex());
+          // Replace the "placeholders" with desired colors.
+          svgString = svgString.replaceAll(logoAcolorLabel, logoAcolor);
+
+          svgString = svgString.replaceAll(logoTcolorLabel, logoTcolor);
+
           return SvgPicture.string(svgString);
         } else if (snapshot.connectionState == ConnectionState.waiting) {
           return const SizedBox();
         } else {
-          return Image.asset(
-              MediaQuery.of(context).platformBrightness == Brightness.light
-                  ? 'assets/icon/ont_logo_square.png'
-                  : 'assets/icon/ont_logo_square_light.png');
+          return Image.asset(Theme.of(context).brightness == Brightness.light
+              ? 'assets/icon/ont_logo_atlas_tracker_white.png'
+              : 'assets/icon/ont_logo_atlas_tracker_dark.png');
         }
       },
     );
